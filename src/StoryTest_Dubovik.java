5fc163ab8cade16f49697430746bafe481988cbe
Albertd7 <albertd7@gmail.com>
2016-09-29 14:33:17 -0400

    Mad lib assignment
    mad lib

---
+++ src/StoryTest_Dubovik.java
@@ -0,0 +1,10 @@
+/**
+ * Dubovik_Story_PA2 Created by Albert Dubovik on 9/29/2016.
+ */
+public class StoryTest_Dubovik
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/Story_Dubovik.java
@@ -0,0 +1,10 @@
+/**
+ * Dubovik_Story_PA2 Created by Albert Dubovik on 9/29/2016.
+ *
+ * This is a program for creating a Mad Lib. It will contain attributes like verb & noun, and a story gotten from the internet.
+ * The user will input for each attribute.
+ */
+public class Story_Dubovik
+{
+    
+}
